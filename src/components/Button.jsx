f185ed5be619b384ae3722158bfbe0aa8ca85f47
ichbintanvir <tanvirnoyon.bubt@gmail.com>
2025-09-20 11:26:34 +0600

    fixed the build in error on Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { cn } from '../../lib/cn'
 import Menu from '../assets/svg/Menu'
 import KitchenItem from '../assets/svg/KitchenItem'
 import Video from '../assets/svg/Video'
-import Instagram from '../assets/svg/instagram'
+import Instagram from '../assets/svg/Instagram'
 
 const Button = ({ children, className, variety = 'btn1', status }) => {
   const baseStyles = 'flex items-center gap-[15px] px-[37px] py-[18px] ring-1 text-black rounded-2xl capitalize cursor-pointer'
